e10c45634b3614b0768eca1962775522a7d406f6
loicC <coissard.loic@gmail.com>
2019-02-18 10:55:19 +0100

    add the pattern publisher listener

--- Scripts/DayDiplomacy_Citizenships.js
+++ Scripts/DayDiplomacy_Citizenships.js
@@ -6,27 +6,43 @@ this.licence = "CC-NC-by-SA 4.0";
 this.description = "This script displays the citizenship.";
 
 /*************************** OXP private functions *******************************************************/
+this._buildID =function (galaxyID systemID){
+
+}
+
 this._F4InterfaceCallback = function (choice) {
     this._resetLinks();
     switch (choice) {
         case "BUY":
-            this._BuyCitizenship();
+            this._buyCitizenship({"galaxyID":system.info.galaxyID, "systemID":system.info.systemID});
             break;
         case "LOSE":
-            this._LoseCitizenship();
+            this._loseCitizenship({"galaxyID":system.info.galaxyID, "systemID":system.info.systemID});
             break;
         default : //"EXIT":
     }
 };
 
+this._myCitizenships=function(){
+    var
+    var message="";
+    var c = this._citizenships;
+    var i = c.length;
+    while (i--){
+        message += systemNameForID(this._citizenships[i])+"\n";
+    }
+    return message;
+}
+
 // citizenship: {"galaxyID"=>galaxyID, "systemID"=>systemID}
 this._buyCitizenship = function(citizenship){
-	var id = citizenship[galaxyID] + " " + citizenship[systemID];
+    var id = citizenship["galaxyID"] + " " + citizenship["systemID"];
     this._citizenships[id]=citizenship;
 }
 
 this._loseCitizenship = function(citizenship){
     //retirer clé :
+    var id = citizenship["galaxyID"] + " " + citizenship["systemID"];
     delete this._citizenships[id];
 }
 
@@ -37,7 +53,7 @@ this._runCitizenship = function () {
         allowInterrupt: true,
         exitScreen: "GUI_SCREEN_INTERFACES",
         choices: {"BUY":"Buy","LOSE":"Lose","EXIT": "Exit"},
-        message: String(system.info.galaxyID) +" "+ String(system.info.systemID)
+         message:this._myCitizenships() //String(system.info.galaxyID) +" "+ String(system.info.systemID)
     };
     mission.runScreen(opts, this._F4InterfaceCallback.bind(this));
 };
@@ -56,10 +72,29 @@ this._initF4Interface = function () {
             callback: this._displayF4Interface.bind(this)
         });
 };
+
+this._publishNewsSubscribers=function(){
+    var myNewSubscriber=this._;
+    var l=myNewSubscriber.length;
+    while (l--)
+    for(subscriber in this._citizenshipsNewsSubscribers){
+        subscriber.$citizenshipsChanged(this._citizenships);
+    }
+}
 /*************************** End OXP private functions ***************************************************/
 
 /*************************** OXP public functions ********************************************************/
+this.$subscribeToCitizenshipsNews=function(){
+    this._citizenshipsNewsSubscribers.push(scriptname); //permet de subscriber
+}
+this.$citizenshipsChanged(citizenships){
 
+}
+
+this.$getSystemsNameByGalaxyAndSystemId = function() {
+    // FIXME perfectperf have the current galaxy saved somewhere?
+    return this._systemsByGalaxyAndSystemId[system.info.galaxyID];
+};
 /*************************** End OXP public functions ****************************************************/
 
 /*************************** Oolite events ***************************************************************/
@@ -74,6 +109,7 @@ this.missionScreenEnded = function () {
 }
 this._startUp = function () {
     var api = this._api = worldScripts.DayDiplomacy_002_EngineAPI;
+    this._systemsByGalaxyAndSystemId = api.$initAndReturnSavedData("systemsByGalaxyAndSystemId", {});
     this._sapi = worldScripts.DayDiplomacy_012_SystemsAPI;
     this._F = api.$getFunctions();
     this._selectedSystemActorId = this._sapi.$getCurrentGalaxySystemsActorIdsBySystemsId()[system.info.systemID]; // FIXME perfectperf?
@@ -83,19 +119,14 @@ this._startUp = function () {
 
     this._citizenships = api.$initAndReturnSavedData("citizenships", {}); // { citizenship => citizenship }
 
+    this._citizenshipsNewsSubscribers=[];
+
     delete this._startUp; // No need to startup twice
 };
 this.startUp = function () {
     worldScripts.DayDiplomacy_000_Engine.$subscribe(this.name);
-
+    this._s.$subscribe(this.name);
     delete this.startUp; // No need to startup twice
 };
 /*************************** End Oolite events ***********************************************************/
-
-
-
-
-
-
-
 
